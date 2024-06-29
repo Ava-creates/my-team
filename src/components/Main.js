49f98d334bac28642cc0df98437953563251a5c9
Avani Tiwari <64213814+Ava-creates@users.noreply.github.com>
2024-06-29 14:34:53 +0530

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ export default function Main() {
     
     return (
     <>
-    <div className="first-page">Main component</div>
+    <div className="first-page">Happy Birthday!</div>
     <svg class="arrows" onClick={scrollToSecondPage}>
               <path class="a1" d="M0 0 L30 32 L60 0"></path>
               <path class="a2" d="M0 20 L30 52 L60 20"></path>
